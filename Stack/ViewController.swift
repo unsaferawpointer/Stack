f6e00f4264a09c04426edc489219d8e75c82ee40
Anton Cherkasov <unsaferawpointer@gmail.com>
2023-08-08 20:34:41 +0300

    Initial Commit

---
+++ Stack/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Stack
+//
+//  Created by Anton Cherkasov on 08.08.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
